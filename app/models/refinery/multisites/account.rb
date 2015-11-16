f47a1dd464cb3f384af74a5fb6740820f2ea40c7
Brice Sanchez <contact@brice-sanchez.com>
2015-11-15 20:49:59 -0500

    Add account model

---
+++ app/models/refinery/multisites/account.rb
@@ -0,0 +1,34 @@
+module Refinery
+  module Multisites
+    class Account < Refinery::Core::BaseModel
+      self.table_name = 'refinery_multisites_accounts'
+
+
+		  before_validation :downcase_subdomain
+
+		  validates :owner, presence: true
+		  validates :subdomain, presence: true,
+		                        uniqueness: { case_sensitive: false },
+		                        format:
+		                        {
+		                          with: /\A[\w\-]+\Z/i,
+		                          message: :invalid_characters
+		                        },
+		                        exclusion:
+		                        {
+		                          in: Refinery::Multisites.restricted_subdomains,
+		                          message: :restricted
+		                        }
+
+		  belongs_to :owner, class_name: "User"
+
+		  has_many :users, inverse_of: :organization
+
+		  private
+
+		  def downcase_subdomain
+		    self.subdomain = subdomain.try(:downcase)
+		  end
+    end
+  end
+end
--- lib/refinery/multisites/configuration.rb
+++ lib/refinery/multisites/configuration.rb
@@ -2,8 +2,10 @@ module Refinery
   module Multisites
 	  include ActiveSupport::Configurable
 
-	  config_accessor :tld_length
+	  config_accessor :tld_length, :restricted_subdomains
+
 
 	  self.tld_length = 1
+	  self.restricted_subdomains = %w(www admin)
 	end
 end
