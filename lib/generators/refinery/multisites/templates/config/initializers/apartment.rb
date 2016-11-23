e443160c45ccc07c2a191235e06b0c067fe6313a
Brice Sanchez <brice@bisscomm.com>
2016-11-23 16:20:35 -0500

    Improve initializers

--- lib/generators/refinery/multisites/templates/config/initializers/apartment.rb
+++ lib/generators/refinery/multisites/templates/config/initializers/apartment.rb
@@ -1,5 +1,13 @@
 Apartment.configure do |config|
   config.tld_length = Refinery::Multisites.tld_length
-  config.excluded_models = ["Refinery::Multisites::Account"]
+  
+  # If you're grafting onto an existing app, add your User class
+  # config.excluded_models = %w( Refinery::Multisites::Account 
+  #   Refinery::Authentication::Devise::Role 
+  #   Refinery::Authentication::Devise::RolesUsers
+  #   Refinery::Authentication::Devise::User 
+  #   Refinery::Authentication::Devise::UserPlugin )
+  config.excluded_models = %w( Refinery::Multisites::Account )
+  
   config.tenant_names = -> { Refinery::Multisites::Account.pluck(:subdomain) }
 end
