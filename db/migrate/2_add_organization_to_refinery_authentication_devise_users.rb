94c4403cbc388902b3139d04ce52d878dc35a159
Brice Sanchez <brice@bisscomm.com>
2016-11-23 16:19:37 -0500

    Fix AddOrganizationToRefineryAuthenticationDeviseUsers migration

--- db/migrate/2_add_organization_to_refinery_authentication_devise_users.rb
+++ db/migrate/2_add_organization_to_refinery_authentication_devise_users.rb
@@ -1,6 +1,6 @@
 class AddOrganizationToRefineryAuthenticationDeviseUsers < ActiveRecord::Migration
   def change
-    if !(user_class = Refinery::Multisites.user_class).nil? && defined?(user_class)
+    unless defined?(User)
       add_column :refinery_authentication_devise_users, :organization_id, :integer
     end
   end
